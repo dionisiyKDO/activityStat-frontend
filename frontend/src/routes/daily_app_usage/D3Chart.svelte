91f12cbcb80cac512dbe9f0a14cf0a62aeb3e928
Denys Kamiskyi <135739796+dionisiyKDO@users.noreply.github.com>
2024-08-15 23:30:20 +0300

    refactor: reorganize code in chart components

--- frontend/src/routes/daily_app_usage/D3Chart.svelte
+++ frontend/src/routes/daily_app_usage/D3Chart.svelte
@@ -4,12 +4,11 @@
     import { onMount } from "svelte";
 
 	let data = $state([]);
-    let waiting = $state('Loading...');
-
 
     let app = $state("chrome.exe");
-    let start_date = $state('2020-01-01');
-    let end_date = $state('2024-08-11');
+    let start_date = $state('2023-02-01');  // TODO: calculate start date automatically
+    let end_date = $state('2024-08-11');    // TODO: set end date to today 
+    
     let filteredData = $derived(
         data.filter(d => {
             const date = new Date(d.timestamp);
@@ -18,100 +17,19 @@
         })
     );
 
-    // TODO: make selector for apps, so i can fetch for different apps
-    // TODO: change back to timescale. Now it scips dates
-    // let chart = $derived(drawChart());
-
-    async function fetchData() {
-        const response = await fetch("/api/daily_app_usage/" + app);
-        const r = JSON.parse( 
-            await response.json(), // returns a promise
-            (key, value) => typeof value === "number" ? Math.round(value * 100) / 100 : value // rounding
-        );
-
-        return r;
-    }
-
-    onMount(() => {
-        fetchData().then(r => {
-            data = r;
-            drawChart();
-        });
-    });
-
-    // TODO: в пробел, с 13 по 20 число, не показывается что 0 часов использовалось приложение, оно просто рисует линию с там 2 часа 13 часа до 4 часа 20 число, не видно нуля в неделю
-    function drawChart() {
-        // set the dimensions and margins of the graph
-        const container = d3.select("#container");
-        const margin = {top: 10, right: 30, bottom: 60, left: 60},
-            width = container.node().getBoundingClientRect().width - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
-
-        // append the svg object to the body of the page
-        d3.select("#chart").selectAll("*").remove();
-        const svg = d3.select("#chart")
-            .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
-            .append("g")
-            .attr("transform", `translate(${margin.left},${margin.top})`);
-
-        // group the data: I want to draw one line per group
-        const sumstat = d3.group(filteredData, d => d.app); // nest function allows to group the calculation per level of a factor
-
-        // Add X axis --> it is a date format
-        const x = d3.scaleTime()
-            .domain(d3.extent(filteredData, function(d) { return new Date(d.timestamp); }))
-            .range([0, width]);
-
-        const xAxis = d3.axisBottom(x)
-            // .ticks(3) // aim for around 10 ticks
-            .tickFormat(d3.timeFormat("%Y-%m-%d"));
-
-        svg.append("g")
-            .attr("transform", `translate(0, ${height})`)
-            .call(xAxis)
-            .selectAll("text")                          // select all labels
-                .attr("transform", "rotate(-20)")       // rotate selected labels
-                .style("text-anchor", "end")            // make so end of the label is at the x-axis line (position anchor is at the end of the label)
-                .style("font-size", "1.1em");
-
-        // Add Y axis
-        const y = d3.scaleLinear()
-            .domain([0, d3.max(filteredData, function(d) { return +d.duration; })])
-            .range([height, 0])
-            .nice();
-
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        // color palette
-        const color = d3.scaleOrdinal()
-            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']);
-
-        // Draw the line
-        svg.selectAll(".line")
-            .data(sumstat)
-            .join("path")
-                .attr("fill", "none")
-                .attr("stroke", function(d){ return color(d[0]); })
-                .attr("stroke-width", 1.5)
-                .attr("d", function(d){
-                    return d3.line()
-                        .x(function(d) { return x(new Date(d.timestamp)); })
-                        .y(function(d) { return y(+d.duration); })
-                        (d[1]);
-                });
-    }
-
     let startDate = $derived(new Date(start_date));
     let endDate = $derived(new Date(end_date));
 
-    // Convert date to a numeric value for the range slider
-    let minDate = new Date('2023-01-01').getTime(); // Minimum date
-    let maxDate = new Date('2025-12-31').getTime(); // Maximum date
+    let minDate = new Date(start_date).getTime(); // Minimum date
+    let maxDate = new Date(end_date).getTime(); // Maximum date
     let startDateNumeric = startDate.getTime();
     let endDateNumeric = endDate.getTime();
 
-    // TODO: мб не перерисовывать полностью граф, а обновлять ось Х, левый правый лимит обновлять
+    // TODO: make input (selector) for avaible apps to include/exclude from the chart
+    // TODO: Consider updating only the X-axis limits instead of fully redrawing the chart
+    // TODO : Add notice (2024-06-13 -- 2024-06-19 doesnt exist)
+        // TODO: Fix chart to display zero usage hours between 13th and 20th, instead of a continuous line from 4h (13th) to 4h (20th)
+
     function updateStartDate(event) {
         startDateNumeric = Number(event.target.value);
         start_date = new Date(startDateNumeric).toISOString().split('T')[0];
@@ -129,6 +47,83 @@
             data = r;
             drawChart();
         });
+    }
+
+    onMount(() => {
+        fetchData().then(r => {
+            data = r;
+            drawChart();
+        });
+    });
+
+    function drawChart() {
+        const container = d3.select("#container");
+        const margin = {top: 10, right: 30, bottom: 60, left: 60};
+        const width = container.node().getBoundingClientRect().width - margin.left - margin.right;
+        const height = 400 - margin.top - margin.bottom;
+
+        // delete old chart
+        d3.select("#chart").selectAll("*").remove();
+
+        const svg = d3.select("#chart")
+            .attr("viewBox", `0 0 ${width + margin.left + margin.right} ${height + margin.top + margin.bottom}`)
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        const sumstat = d3.group(filteredData, d => d.app); // group the data by app: I want to draw one line per app
+        const color = d3.scaleOrdinal() // create color palette for different lines
+            .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']);
+
+        // xAxis
+        const xScale = d3.scaleTime()
+            .domain(d3.extent(filteredData, function(d) { return new Date(d.timestamp); }))
+            .range([0, width]);
+
+        const xAxis = d3.axisBottom(xScale)
+            .tickFormat(d3.timeFormat("%Y-%m-%d"));
+
+        svg.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(xAxis)
+            .selectAll("text")
+                .attr("transform", "rotate(-20)")
+                .style("text-anchor", "end")
+                .style("font-size", "1.1em");
+
+        // yAxis
+        const yScale = d3.scaleLinear()
+            .domain([0, d3.max(filteredData, function(d) { return +d.duration; })])
+            .range([height, 0])
+            .nice();
+
+        const yAxis = d3.axisLeft(yScale)
+
+        svg.append("g")
+            .call(yAxis);
+
+        // Draw the line
+        svg.selectAll(".line")
+            .data(sumstat)
+            .join("path")
+                .attr("fill", "none")
+                .attr("stroke", function(d){ return color(d[0]); })
+                .attr("stroke-width", 1.5)
+                .attr("d", function(d){
+                    return d3.line()
+                        .x(function(d) { return xScale(new Date(d.timestamp)); })
+                        .y(function(d) { return yScale(+d.duration); })
+                        (d[1]);
+                });
+    }
+
+    async function fetchData() {
+        const response = await fetch("/api/daily_app_usage/" + app);
+        const r = JSON.parse( 
+            await response.json(),
+            (key, value) => typeof value === "number" ? Math.round(value * 100) / 100 : value // round numbers to 2 decimals
+        );
+
+        return r;
     }
 </script>
 
--- frontend/src/routes/spent_time/D3Chart.svelte
+++ frontend/src/routes/spent_time/D3Chart.svelte
@@ -7,8 +7,8 @@
 
     // TODO : loading something for first request
     // TODO : Add notice (2024-06-13 -- 2024-06-19 doesnt exist)
+    // TODO : add the ability to remove specific apps from the chart
     onMount(() => {
-        // Setting up the SVG container dimensions
         const container = d3.select("#chart-container");
         const width = container.node().getBoundingClientRect().width;
         const height = 400;
@@ -21,45 +21,44 @@
                 .append("g");                                        // append a group element
         
         // Create a scale for the x-axis (applications)
-        const x = d3.scaleBand()
+        const xScale = d3.scaleBand()
             .domain(data.map(d => d.title))                          // select data
             .range([margin.left, width - margin.right])              // set range
             .padding(0.05);                                          // add padding
 
         // Create a scale for the y-axis (duration)
-        const y = d3.scaleLinear()
+        const yScale = d3.scaleLinear()
             .domain([0, d3.max(data, d => d.duration)])              // select data
             .range([height - margin.bottom, margin.top])             // set range
             .nice();                                                 // set highest y value a nice round number 
 
-        // Add the axises to the SVG
-        // #region
         // Add the x-axis to the SVG
+        const xAxis = d3.axisBottom(xScale)
         svg.append("g")
             .attr("transform", `translate(0,${height - margin.bottom})`)    // y-axis position
-            .call(d3.axisBottom(x))                                         // x-axis
+            .call(xAxis)                                                    // x-axis
             .selectAll("text")                          // select all labels
                 .attr("transform", "rotate(-40)")       // rotate selected labels
                 .style("text-anchor", "end")            // make so end of the label is at the x-axis line (position anchor is at the end of the label)
                 .style("font-size", "1.1em");
 
         // Add the y-axis to the SVG
+        const yAxis = d3.axisLeft(yScale)
         svg.append("g")
             .attr("transform", `translate(${margin.left},0)`)   // x-axis position
-            .call(d3.axisLeft(y));                              // y-axis                    
-        // #endregion
+            .call(yAxis);                                       // y-axis                    
 
         // Create the bars
         svg.selectAll(".bar")                           // select all (future) bars
             .data(data)                                 // bind data ('create' bars for each entrie in data)
             .enter().append("rect")                     // append a 'rect' for each entrie and for each rect:
-                .attr("class", "bar")                       // add class 'bar'
-                .attr("x", d => x(d.title))                   // set x position
-                .attr("y", d => y(d.duration))              // set y position
-                .attr("width", x.bandwidth())               // set width
-                .attr("height", d => y(0) - y(d.duration))  // set height
-                .attr("fill", "#e9e9e9")                    // set color
-                .attr("transition", "all 0.5s ease")             // add transition
+                .attr("class", "bar")                                   // add class 'bar'
+                .attr("x", d => xScale(d.title))                        // set x position
+                .attr("y", d => yScale(d.duration))                     // set y position
+                .attr("width", xScale.bandwidth())                      // set width
+                .attr("height", d => yScale(0) - yScale(d.duration))    // set height
+                .attr("fill", "#e9e9e9")                                // set color
+                .attr("transition", "all 0.5s ease")                    // add transition
             .on("mouseover", function(event, d) {
                 tooltip
                     .transition()
@@ -88,7 +87,6 @@
                     .attr("cursor", "default");
             });
         
-        // Add a tooltip
         const tooltip = d3.select("body")
             .append("div")
             .attr("class", "tooltip")
@@ -100,34 +98,17 @@
                 .style("border-radius", "2px")
                 .style("opacity", 0)
                 .style("pointer-events", "none"); // Prevent the tooltip from interfering with mouse events
-
     });
 </script>
+
 
 <div id="chart-container">
     <svg id="spent-time-chart"></svg>
 </div>
 
+
 <style>
     svg {
         font: 10px sans-serif;
     }
-
-    /* .axis path,
-    .axis line {
-        fill: none;
-        shape-rendering: crispEdges;
-    }
-    .tooltip {
-        position: absolute;
-        text-align: center;
-        width: auto;
-        height: auto;
-        padding: 5px;
-        font: 12px sans-serif;
-        background: lightsteelblue;
-        border: 0px;
-        border-radius: 8px;
-        pointer-events: none;
-    } */
 </style>
