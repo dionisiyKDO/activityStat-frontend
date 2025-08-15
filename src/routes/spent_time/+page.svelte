7ae6c37113f508dd1bc18bc0106343736f0ac265
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-15 04:37:37 +0300

    refactoring page

--- src/routes/spent_time/+page.svelte
+++ src/routes/spent_time/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import BarChart from '$lib/SpentTime/BarChart.svelte';
+	import Table from '$lib/SpentTime/Table.svelte';
 	import { 
         fetchSpentTime, 
         fetchMetadata, 
@@ -6,46 +8,35 @@
         type Metadata 
     } from './load';
 
-	import D3Chart from './D3Chart.svelte';
-	import Table from './Table.svelte';
-	import { onMount } from 'svelte';
-
-	let trackStatsReq: Promise<SpentTime[] | null>;
-	let metadataReq: Promise<Metadata | null>;
-
-	onMount(() => {
-		trackStatsReq = fetchSpentTime();
-		metadataReq = fetchMetadata();
-	});
+    // Requests
+	let trackStatsReq: Promise<SpentTime[] | null> = fetchSpentTime();
+	let metadataReq: Promise<Metadata | null> = fetchMetadata();
 </script>
+
+{#await metadataReq}
+    <p class="loading">Loading metadata...</p>
+{:then metadata}
+
+    <div class="flex gap-4 m-4">
+        <h1>Spent Time</h1>
+        {#if metadata != null}
+            <p class="">Start date: {metadata.start_date}</p>
+            <p class="">End date: {metadata.end_date}</p>
+            <p class="">Total records: {metadata.total_records}</p>
+        {/if}
+    </div>
+{:catch error}
+    <p class="error">{error.message}</p>
+{/await}
+
 
 {#await trackStatsReq}
 	<p class="loading">Loading list...</p>
 {:then data}
 	{#if data != null}
-        {#await metadataReq}
-            <p class="loading">Loading metadata...</p>
-        {:then metadata}
-        
-            <div class="flex gap-4 m-4">
-                <h1>Spent Time</h1>
-                {#if metadata != null}
-                    <p class="">Start date: {metadata.start_date}</p>
-                    <p class="">End date: {metadata.end_date}</p>
-                    <p class="">Total records: {metadata.total_records}</p>
-                {/if}
-            </div>
-
-            <D3Chart {data} />
-            <Table {data} />
-        {:catch error}
-            <p class="error">{error.message}</p>
-        {/await}
+        <BarChart {data} />
+        <Table {data} />
     {/if}
 {:catch error}
 	<p class="error">{error.message}</p>
 {/await}
-
-
-<style>
-</style>
