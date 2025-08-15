7e22a76230ae777f8c19538f05cff89778818fee
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-15 04:32:53 +0300

    refactor spent time fetches

--- src/routes/spent_time/load.ts
+++ src/routes/spent_time/load.ts
@@ -5,25 +5,27 @@ export interface SpentTime {
 }
 
 export interface Metadata {
-	start_date: string
-	end_date: string
+	start_date: string | Date
+	end_date: string | Date
 	total_records: number
 }
   
 
 export async function fetchSpentTime(): Promise<SpentTime[] | null> {
 	try {
-		const response = await fetch("/api/spent_time");
+        const response = await fetch(
+            `/api/spent_time`
+        );
 
 		if (!response.ok) {
 			const data = await response.json();
-			const error = data.error || 'Failed to fetch tracks';
+			const error = data.error || 'Failed to fetch time';
 			console.log(error);
 			return null;
 		}
 
-		const data = (await response.json());
-		return JSON.parse(data);
+		const data: SpentTime[] = await response.json();
+		return data;
 	} catch (err) {
 		console.log(err);
 		return null;
@@ -33,22 +35,25 @@ export async function fetchSpentTime(): Promise<SpentTime[] | null> {
 
 export async function fetchMetadata(): Promise<Metadata | null> {
 	try {
-		const response = await fetch("/api/dataset_metadata");
+        const response = await fetch(
+            `/api/dataset_metadata`
+        );
 
 		if (!response.ok) {
 			const data = await response.json();
-			const error = data.error || 'Failed to fetch tracks';
+			const error = data.error || 'Failed to fetch metadata';
 			console.log(error);
 			return null;
 		}
 
-		let data = (await response.json()) as Metadata;
+		let data: Metadata = await response.json();
+
 		data.start_date = new Date(data.start_date).toLocaleDateString();
 		data.end_date = new Date(data.end_date).toLocaleDateString();
+		
 		return data; 
 	} catch (err) {
 		console.log(err);
 		return null;
 	}
 }
-
