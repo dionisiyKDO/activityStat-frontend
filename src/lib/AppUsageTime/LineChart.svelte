f11ada9c4c88bbd71bc34eebe8723269f53fa6e8
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-17 06:44:33 +0300

    add more colors

--- src/lib/AppUsageTime/LineChart.svelte
+++ src/lib/AppUsageTime/LineChart.svelte
@@ -39,15 +39,24 @@
 	const colorScale = d3
 		.scaleOrdinal<string>()
 		.range([
-			'#e41a1c',
-			'#377eb8',
-			'#4daf4a',
-			'#984ea3',
-			'#ff7f00',
-			'#ffff33',
-			'#a65628',
-			'#f781bf',
-			'#999999'
+			// 16 distinct colors
+			'#1f77b4',
+			'#ff7f0e',
+			'#2ca02c',
+			'#d62728',
+			'#9467bd',
+			'#8c564b',
+			'#e377c2',
+			'#7f7f7f',
+			'#bcbd22',
+			'#17becf',
+			'#aec7e8',
+			'#ffbb78',
+			'#98df8a',
+			'#ff9896',
+			'#c5b0d5',
+			'#c49c94',
+			'#f7b6d2'
 		]);
 
 	let animDuration = 0; // better keep it zero for performance
