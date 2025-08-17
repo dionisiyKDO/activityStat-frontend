b1a97bbcc26c975f43ee3881fcf295da08d194d1
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-17 06:44:06 +0300

    adapt fetching to new backend endpoint

--- src/routes/daily_app_usage/load.ts
+++ src/routes/daily_app_usage/load.ts
@@ -2,33 +2,36 @@ import { type RawAppUsageData, type AppUsageData, type App, type margin } from '
 
 export async function fetchAppUsageData(apps: App[]): Promise<AppUsageData[] | null> {
 	try {
-		let results: AppUsageData[] = [];
+		if (apps.length === 0) return [];
 
-		for (const app of apps) {
-			const response = await fetch(`/api/daily_app_usage/${app.title}`);
+		const response = await fetch(`/api/daily_app_usage`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				app_titles: apps.map(app => app.title)
+			})
+		});
 
-			if (!response.ok) {
-				const data = await response.json();
-				const error = data.error || 'Failed to fetch app';
-				console.log(error);
-				return null;
-			}
-
-			const raw_data: RawAppUsageData[] = await response.json();
-
-			let data: AppUsageData[] = raw_data.map((d: any) => {
-				return {
-					timestamp: d.date,
-					date: new Date(d.date),
-					app: app.title,
-					duration: d.duration
-				};
-			});
-
-			results.push(...data);
+		if (!response.ok) {
+			const data = await response.json();
+			const error = data.error || 'Failed to fetch app';
+			console.log(error);
+			return null;
 		}
 
-		return results;
+		const raw_data: RawAppUsageData[] = await response.json();
+		let data: AppUsageData[] = raw_data.map((d: any) => {
+			return {
+				timestamp: d.date,
+				date: new Date(d.date),
+				app: d.app,
+				duration: d.duration
+			};
+		});
+
+		return data;
 	} catch (err) {
 		console.log(err);
 		return null;
