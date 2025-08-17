1750bd57b7a0bcd14aa36494cbc05f79b1e8a9b6
Denys Kaminskyi <dionisiy.kdo@gmail.com>
2025-08-17 10:37:47 +0300

    Update header links

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -4,13 +4,13 @@
 	<nav class="flex items-center">
 		<ul class="flex list-none gap-4">
 			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
 				<a href="/spent_time">Spent time</a>
 			</li>
 			<li>
 				<a href="/daily_app_usage">Daily app usage</a>
+			</li>
+			<li>
+				<a href="/daily_os_usage">Daily OS usage</a>
 			</li>
 		</ul>
 	</nav>
